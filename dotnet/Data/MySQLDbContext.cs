29bce5a51c103c3c7c43638a4cefd1df0c218610
DTB2324 <dotrongbinh20012004@gmail.com>
2026-01-12 17:00:31 +0700

    init project

---
+++ dotnet/Data/MySQLDbContext.cs
@@ -0,0 +1,27 @@
+﻿using dotnet.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet.Data
+{
+    public class MySQLDbContext : DbContext
+    {
+        public MySQLDbContext(DbContextOptions<MySQLDbContext> options) : base(options)
+        {
+        }
+
+        // Thêm key bởi khi không sử dụng Id gốc ở Entity, EF Core không tự động nhận diện được
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<CourseNotification>()
+            //    .HasKey(cn => new { cn.CourseId, cn.NotificationId });
+
+            //modelBuilder.Entity<Course>()
+            //    .HasIndex(c => new { c.InstructorId, c.Title })
+            //    .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<SetUp> SetUps { get; set; }
+    }
+}
---
+++ dotnet/Domains/SetUp.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet.Domains
+{
+    public class SetUp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
